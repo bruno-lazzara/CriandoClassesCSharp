251113b7d8a6c86ce5c449a352025fcfb9b217ac
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2022-02-10 15:17:49 -0300

    Atualizações da classe ContaCorrente

--- 04-ByteBank/ContaCorrente.cs
+++ 04-ByteBank/ContaCorrente.cs
@@ -6,9 +6,9 @@ public class ContaCorrente
     public string titular;
     public int agencia;
     public int numero;
-    public double saldo = 100; // Atribuído um valor padrão para uma variável dentro da classe.
+    public double saldo = 100; // Atribuído um valor padrão para um atributo dentro da classe.
 
-    // Função Sacar. Esta função precisa retornar um bool (se o saque foi possível ou não)
+    // Função (Método) Sacar. Esta função precisa retornar um bool (se o saque foi possível ou não)
     // e precisa receber um argumento, que é o valor que o usuário deseja sacar.
     public bool Sacar(double valor)
     {
@@ -16,11 +16,33 @@ public class ContaCorrente
         {
             return false;
         }
-        else
+
+        this.saldo -= valor;
+        return true;
+    }
+
+    // Esse método não tem retorno (void), não devolve nada após sua execução.
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+    // É comum chamarmos de função quando há retorno na função, e de método quando não há retorno,
+    // mas as duas formas estão corretas.
+
+    // Essa função recebe como argumentos o valor a ser transferido, e a conta a ser depositado esse valor
+    // (que é um objeto do tipo ContaCorrente).
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
         {
-            this.saldo -= valor;
-            return true;
+            return false; // O return para imediatamente a execução do código.
         }
+
+        this.saldo -= valor;
+        // Podemos chamar o método Depositar para realizar essa operação, sem precisarmos escrever
+        // mais código para somar o valor ao saldo da outra conta corrente.
+        contaDestino.Depositar(valor); // contaDestino.saldo += valor
+        return true;
     }
 }
 
--- 04-ByteBank/Program.cs
+++ 04-ByteBank/Program.cs
@@ -16,10 +16,34 @@ namespace _04_ByteBank
 
             Console.WriteLine(contaDoBruno.saldo); // 100 (valor padrão atribuído na classe)
 
-            bool resultadoSaque = contaDoBruno.Sacar(50);
+            bool resultadoSaque = contaDoBruno.Sacar(500); // Guarda em uma variável o retorno da função Sacar
 
-            Console.WriteLine(contaDoBruno.saldo); // 50
-            Console.WriteLine(resultadoSaque); // True. O saque foi permitido
+            Console.WriteLine(contaDoBruno.saldo); // 100
+            Console.WriteLine(resultadoSaque); // False. O saque não foi permitido
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo); // 600
+
+            // Criando um novo objeto e atribuindo o nome do titular de uma maneira diferente.
+            ContaCorrente contaDaGabriela = new ContaCorrente
+            {
+                titular = "Gabriela"
+            };
+
+            Console.WriteLine("Saldo do Bruno :" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela :" + contaDaGabriela.saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno :" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela :" + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado da transferência: " + resultadoTransferencia);
+
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine("Saldo do Bruno :" + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela :" + contaDaGabriela.saldo);
 
             Console.ReadLine();
         }
